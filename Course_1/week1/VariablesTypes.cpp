09a854ebc9c62bf95ee95494cfa5be8de6f19b86
Gooogr <gusarovga@ya.ru>
2020-06-25 00:09:30 +0300

    Update HelloProject.cpp, VariablesTypes, and VariablesTypes.cpp

---
+++ Course_1/week1/VariablesTypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string> // Библиотека для использования переменных типа string
+#include <vector> // Библиотека для использования переменных типа vector
+using namespace std;
+
+int main(){
+	int x = -5;
+	double pi = 3.14;
+	bool logical_value = true;
+	char symbol = 'Z';
+	string hw = "Hello world";
+	vector<int> nums = {1, 3, 5, 7};
+	cout << nums.size();
+	return 0;
+}
