67877a26f9b250efac44b5317d0ac350a816497a
Gooogr <gusarovga@ya.ru>
2020-06-27 13:55:38 +0300

    Add InputAndOutput.cpp

---
+++ Course_1/week1/InputAndOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+	//~ int x, y;
+	//~ cin >> x >> y;
+	//~ cout << x + y;
+	
+	string name;
+	cin >> name;
+	cout << "Hello, " <<name;
+	return 0;
+}
